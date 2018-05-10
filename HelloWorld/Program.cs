61012d2eca768730ed8cf413e20c6a2a86c2f3a0
Harry Mumford-Turner <harrymt@me.com>
2018-05-10 16:54:43 +0100

    Added Strings Section

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,6 +6,15 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            Strings s = new Strings();
+            s.Interpolation();
+            Console.Read();
+        }
+    }
+
+    class Strings {
+        public void Interpolation()
+        {
             var name = "Harry";
             Console.WriteLine($"Hello everyone, my name is {name}!");
         }
