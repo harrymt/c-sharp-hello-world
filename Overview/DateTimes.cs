cbda60fded72ae3081adba3f79a276fabbd68862
Harry Mumford-Turner <harrymt@me.com>
2018-05-10 17:45:49 +0100

    Finished off datetimes

--- Overview/DateTimes.cs
+++ Overview/DateTimes.cs
@@ -5,6 +5,8 @@ class DateTimes
     public DateTimes()
     {
         DefineTimes();
+        PartsOfDateTime();
+        CalculateDifferences();
     }
 
     void DefineTimes()
@@ -14,5 +16,22 @@ class DateTimes
         DateTime birthday = new DateTime(1993, 11, 16);
         DateTime tomorrow = DateTime.Today.AddDays(1);
         DateTime parsed = DateTime.Parse("2018/11/12");
+    }
+
+    void PartsOfDateTime()
+    {
+        DateTime now = DateTime.Now;
+        Console.WriteLine($"It is the year {now.Year}");
+        Console.WriteLine($"What a month {now.Month}");
+        Console.WriteLine($"To be alive {now.Day}");
+        Console.WriteLine($"At this time {now.Hour} {now.Minute} {now.Second}");
+    }
+
+    void CalculateDifferences()
+    {
+        DateTime tomorrow = DateTime.Today.AddDays(1);
+        DateTime today = DateTime.Today;
+        TimeSpan difference = today - tomorrow;
+        Console.WriteLine($"The difference between today and tomorrow is {difference.TotalDays} day.");
     }
 }
