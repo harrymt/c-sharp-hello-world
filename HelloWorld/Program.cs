1283f35ebf6896bbf04b4e1bef624d593c8c56e6
Harry Mumford-Turner <harrymt@me.com>
2018-05-10 16:59:21 +0100

    Strings start as null

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,17 +6,32 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Strings s = new Strings();
-            s.Interpolation();
+            new Strings();
             Console.Read();
         }
     }
 
     class Strings {
+        public Strings()
+        {
+            Interpolation();
+            StartsNull();
+        }
+
         public void Interpolation()
         {
             var name = "Harry";
             Console.WriteLine($"Hello everyone, my name is {name}!");
+
+            string anotherName = "John";
+            Console.WriteLine($"My surname is {anotherName}");
+        }
+        
+        public void StartsNull()
+        {
+            string startsNull; // null
+            startsNull = "a value";
+            Console.WriteLine($"Strings start null, but can be assigned {startsNull}");
         }
     }
 }
