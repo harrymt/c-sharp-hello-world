0175b19dd5907b37a6ee4139be961fa3dd5e7394
Harry Mumford-Turner <harrymt@me.com>
2018-05-10 17:28:17 +0100

    Rename to Overview

---
+++ HelloWorld/DateTimes.cs
@@ -0,0 +1,7 @@
+﻿class DateTimes
+{
+    public DateTimes()
+    {
+
+    }
+}
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,79 +1,12 @@
 ﻿using System;
 using System.Text;
 
-namespace HelloWorld
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            new Strings();
-            Console.Read();
-        }
-    }
-
-    class Strings {
-        public Strings()
-        {
-            StartsNull();
-            EmptyStrings();
-            Interpolation();
-            Concatenation();
-            UseStringBuilderForComplexConcats();
-            HasNormalBuiltIns();
-        }
-        
-        public void StartsNull()
-        {
-            string startsNull; // null
-            startsNull = "a value";
-            Console.WriteLine($"Strings start null, but can be assigned {startsNull}");
-        }
-
-        public void EmptyStrings()
-        {
-            string empty = "";
-            string alsoEmpty = String.Empty;
-            Console.WriteLine($"Use String.Empty for empty strings {empty == alsoEmpty}");
-        }
-
-        public void Interpolation()
-        {
-            var name = "Harry";
-            Console.WriteLine($"Hello everyone, my name is {name}!");
-
-            string anotherName = "John";
-            Console.WriteLine($"My surname is {anotherName}");
-        }
-
-        public void Concatenation()
-        {
-            string name = "Harry";
-            string joiner = " is ";
-            string age = "24 years";
-            string sentence = name + joiner + age;
-            Console.WriteLine(sentence);
-        }
-
-        public void UseStringBuilderForComplexConcats()
-        {
-            string name = "Harry";
-            string surname = "Mumford-Turner";
-            string dog = "bernie";
-            StringBuilder sb = new StringBuilder();
-            sb.Append(name)
-                .Append(surname)
-                .Append(dog);
-            Console.WriteLine(sb.ToString());
-        }
-
-        public void HasNormalBuiltIns()
-        {
-            string upper = "make me upper case".ToUpper();
-            string lower = "MAKE ME LOWER CASE".ToLower();
-            string has = " has ".Trim();
-            string usual = "usual   ".TrimEnd();
-            string padding = " padding functions".TrimStart();
-        }
+        new Strings();
+        new DateTimes();
+        Console.Read();
     }
 }
