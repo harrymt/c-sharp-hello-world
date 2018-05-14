aaa206f51614d7ca9dcd50bdf1e92e9e8735ff7b
Harry Mumford-Turner <harrymt@me.com>
2018-05-14 10:23:59 +0100

    Added ref and out types

--- Overview/Strings.cs
+++ Overview/Strings.cs
@@ -1,74 +1,76 @@
 ﻿using System;
 using System.Text;
 
-
-class Strings
+namespace Overview
 {
-    public Strings()
+    class Strings
     {
-        StartsNull();
-        EmptyStrings();
-        Interpolation();
-        Concatenation();
-        UseStringBuilderForComplexConcats();
-        HasNormalBuiltIns();
-    }
-
-    void StartsNull()
-    {
-        string startsNull; // null
-        startsNull = "a value";
-        Console.WriteLine($"Strings start null, but can be assigned {startsNull}");
-    }
-
-    void EmptyStrings()
-    {
-        string empty = "";
-        string alsoEmpty = String.Empty;
-        Console.WriteLine($"Use String.Empty for empty strings {empty == alsoEmpty}");
-
-        if (String.IsNullOrEmpty(empty))
+        public Strings()
         {
-            Console.WriteLine($"Static methods for checking if strings are both null or empty");
+            StartsNull();
+            EmptyStrings();
+            Interpolation();
+            Concatenation();
+            UseStringBuilderForComplexConcats();
+            HasNormalBuiltIns();
         }
-    }
 
-    void Interpolation()
-    {
-        var name = "Harry";
-        Console.WriteLine($"Hello everyone, my name is {name}!");
+        void StartsNull()
+        {
+            string startsNull; // null
+            startsNull = "a value";
+            Console.WriteLine($"Strings start null, but can be assigned {startsNull}");
+        }
 
-        string anotherName = "John";
-        Console.WriteLine($"My surname is {anotherName}");
-    }
+        void EmptyStrings()
+        {
+            string empty = "";
+            string alsoEmpty = String.Empty;
+            Console.WriteLine($"Use String.Empty for empty strings {empty == alsoEmpty}");
 
-    void Concatenation()
-    {
-        string name = "Harry";
-        string joiner = " is ";
-        string age = "24 years";
-        string sentence = name + joiner + age;
-        Console.WriteLine(sentence);
-    }
+            if (String.IsNullOrEmpty(empty))
+            {
+                Console.WriteLine($"Static methods for checking if strings are both null or empty");
+            }
+        }
 
-    void UseStringBuilderForComplexConcats()
-    {
-        string name = "Harry";
-        string surname = "Mumford-Turner";
-        string dog = "bernie";
-        StringBuilder sb = new StringBuilder();
-        sb.Append(name)
-            .Append(surname)
-            .Append(dog);
-        Console.WriteLine(sb.ToString());
-    }
+        void Interpolation()
+        {
+            var name = "Harry";
+            Console.WriteLine($"Hello everyone, my name is {name}!");
 
-    void HasNormalBuiltIns()
-    {
-        string upper = "make me upper case".ToUpper();
-        string lower = "MAKE ME LOWER CASE".ToLower();
-        string has = " has ".Trim();
-        string usual = "usual   ".TrimEnd();
-        string padding = " padding functions".TrimStart();
+            string anotherName = "John";
+            Console.WriteLine($"My surname is {anotherName}");
+        }
+
+        void Concatenation()
+        {
+            string name = "Harry";
+            string joiner = " is ";
+            string age = "24 years";
+            string sentence = name + joiner + age;
+            Console.WriteLine(sentence);
+        }
+
+        void UseStringBuilderForComplexConcats()
+        {
+            string name = "Harry";
+            string surname = "Mumford-Turner";
+            string dog = "bernie";
+            StringBuilder sb = new StringBuilder();
+            sb.Append(name)
+                .Append(surname)
+                .Append(dog);
+            Console.WriteLine(sb.ToString());
+        }
+
+        void HasNormalBuiltIns()
+        {
+            string upper = "make me upper case".ToUpper();
+            string lower = "MAKE ME LOWER CASE".ToLower();
+            string has = " has ".Trim();
+            string usual = "usual   ".TrimEnd();
+            string padding = " padding functions".TrimStart();
+        }
     }
 }
--- Overview/Types.cs
+++ Overview/Types.cs
@@ -5,11 +5,32 @@ class Types
     public Types()
     {
         GetCodeNameWithNameOf();
+        ByRef();
     }
 
     void GetCodeNameWithNameOf()
     {
         Console.WriteLine(nameof(Types));
         Console.WriteLine(nameof(GetCodeNameWithNameOf));
+    }
+
+    void ByRef()
+    {
+        string data = "Original data";
+        ChangeString(data);
+        Console.WriteLine(data); // Original data
+
+        ChangeString(ref data);
+        Console.WriteLine(data == null); // data is now a null reference
+    }
+
+    static void ChangeString(string str)
+    {
+        str = null;
+    }
+
+    static void ChangeString(ref string str)
+    {
+        str = null;
     }
 }
